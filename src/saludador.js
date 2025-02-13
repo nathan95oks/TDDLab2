ae1ff97f995d461c59164afc62d6b8099f37db58
nathan95oks <nathanaelalba75@gmail.com>
2025-02-13 11:33:36 -0400

    saludo con hora

--- src/saludador.js
+++ src/saludador.js
@@ -1,6 +1,18 @@
 function saludar(nombre) {
-    if(nombre){
-        return `Bienvenido , ${nombre}`;
+
+    const fecha = new Date();
+    const hora=fecha.getHours();
+
+    if((nombre)){
+    if(hora > 6 && hora < 12){
+        return `Buenos Dias , ${nombre}`;
     }
-  };
+    else if(hora>12 && hora<18 ){
+        return `Buenas Tardes , ${nombre}`;
+    }
+    else{
+        return `Buenas Noches,${nombre}`;
+    }
+    } 
+}
   export default saludar;
