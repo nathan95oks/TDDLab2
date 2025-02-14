24ed797759272df7b4b8fa6a426984b15c7d91ab
nathan95oks <nathanaelalba75@gmail.com>
2025-02-13 23:24:40 -0400

    Saludador con nombre hora genero edad e idioma

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import saludar from "./saludador";
 const nombreInput = document.querySelector("#nombre");
 const generoSelect = document.querySelector("#genero");
 const edadInput =document.querySelector("#edad");
+const idiomaSelect = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -13,8 +14,9 @@ form.addEventListener("submit", (event) => {
   const nombre = nombreInput.value.trim();
   const genero = generoSelect.value;
   const edad = parseInt(edadInput.value);
+  const idioma = idiomaSelect.value;
 
-  const saludo = saludar(nombre, genero,edad);
+  const saludo = saludar(nombre, genero,edad,idioma);
   
   div.innerHTML = `<p>${saludo}</p>`;
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,9 +1,9 @@
-function saludar(nombre,genero,edad) {
+function saludar(nombre,genero,edad,idioma) {
 
     const fecha = new Date();
     const hora=fecha.getHours();
 
-    if((nombre)){
+    if((idioma==='espanol')){
 
         let saludo='';
     if((genero==='masculino')&&(edad<30 && edad >0)){
@@ -26,6 +26,30 @@ function saludar(nombre,genero,edad) {
     }
     else{
         return `Buenas Noches  ${saludo} ${nombre}`;
+    }
+  }
+  
+  else {
+    if((genero==='masculino')&&(edad<30 && edad >0)){
+        saludo='Young Man';
+    }else if((genero ==='masculino')&&(edad >30)){
+        saludo='Sir.';  
+    }
+    if((genero ==='femenino')&&(edad<30 && edad >0)){
+        saludo = 'Young Lady';
+    }else if((genero ==='femenino')&&(edad >30)){
+        saludo ='Miss.';
+    }
+   
+
+    if(hora > 6 && hora < 12){
+        return `Good Morning   ${saludo} ${nombre}`;
+    }
+    else if(hora>12 && hora<18 ){
+        return `Good Afternoon  ${saludo} ${nombre}`;
+    }
+    else{
+        return `Good Evening  ${saludo} ${nombre}`;
     }
   } 
 }
