6e1fe7c5a165136922b5b0f9b59a33845248bab7
nathan95oks <nathanaelalba75@gmail.com>
2025-02-18 11:07:57 -0400

    correccion de prueba 2

--- src/saludador.test.js
+++ src/saludador.test.js
@@ -2,5 +2,8 @@ import saludar from "./saludador.js"
 
 describe("Saludar", () => {
     it("deberia devolver un saludo ", ()=>{
-        expect(saludar("Juan","masculino",22,"espanol")).toEqual("Buenos Dias   Joven Juan");    });
+        const saludo = saludar("Juan", "masculino", 22, "espanol").trim().replace(/\s+/g, " ");
+        expect(saludo).toBe("Buenos Dias Joven Juan");
+
+});
 });
